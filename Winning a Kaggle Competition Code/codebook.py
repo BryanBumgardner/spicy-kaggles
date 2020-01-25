5e58090e870ab64d0f988ec172ed32bda37a5f43
Bryan Bumgardner <bryanbumgardner@gmail.com>
2020-01-25 13:23:09 -0500

    adding some Random code from datacamp's kaggle competition thing so I can preserve the flow

--- Winning a Kaggle Competition Code/codebook.py
+++ Winning a Kaggle Competition Code/codebook.py
@@ -9,3 +9,199 @@ rf = RandomForestRegressor()
 
 # Train a model
 rf.fit(X=train[['store', 'item']], y=train['sales'])
+
+# Read test and sample submission data
+test = pd.read_csv('test.csv')
+sample_submission = pd.read_csv('sample_submission.csv')
+
+# Show the head() of the sample_submission
+print(sample_submission.head())
+
+# Get predictions for the test set
+test['sales'] = rf.predict(test[['store', 'item']])
+
+# Write test predictions using the sample_submission format
+test[['id', 'sales']].to_csv('kaggle_submission.csv', index=False)
+
+# Overfitting and learning XGBoost
+# Use MSE to see if we're overfitting 
+
+import xgboost as xgb
+
+# Create DMatrix on train data
+dtrain = xgb.DMatrix(data=train[['store', 'item']],
+                     label=train['sales'])
+
+# Define xgboost parameters
+params = {'objective': 'reg:linear',
+          'max_depth': 2,
+          'silent': 1}
+
+# Train xgboost model
+xg_depth_2 = xgb.train(params=params, dtrain=dtrain)
+
+
+from sklearn.metrics import mean_squared_error
+
+dtrain = xgb.DMatrix(data=train[['store', 'item']])
+dtest = xgb.DMatrix(data=test[['store', 'item']])
+
+# For each of 3 trained models
+for model in [xg_depth_2, xg_depth_8, xg_depth_15]:
+    # Make predictions
+    train_pred = model.predict(dtrain)     
+    test_pred = model.predict(dtest)          
+    
+    # Calculate metrics
+    mse_train = mean_squared_error(train['sales'], train_pred)                  
+    mse_test = mean_squared_error(test['sales'], test_pred)
+    print('MSE Train: {:.3f}. MSE Test: {:.3f}'.format(mse_train, mse_test))
+
+
+
+
+"""
+Solution workflow:
+1. Understand the problem
+- Data type?
+- Problem type? Regression, classification, ranking
+- Evaluation metric (what we're being graded on) ROC AUC, F1 Score, etc
+
+
+
+
+2. EDA
+3. Local validation? Prevent overfitting
+4. Modeling
+
+"""
+
+import numpy as np
+
+# Import MSE from sklearn
+from sklearn.metrics import mean_squared_error
+
+# Define your own MSE function
+def own_mse(y_true, y_pred):
+  	# Raise differences to the power of 2
+    squares = np.mean_squared_error(y_true - y_pred, 2)
+    # Find mean over all observations
+    err = np.mean_squared_error(squares)
+    return err
+
+print('Sklearn MSE: {:.5f}. '.format(mean_squared_error(y_regression_true, y_regression_pred)))
+print('Your MSE: {:.5f}. '.format(own_mse(y_regression_true, y_regression_pred)))
+
+
+import numpy as np
+
+# Import log_loss from sklearn
+from sklearn.metrics import log_loss
+
+# Define your own LogLoss function
+def own_logloss(y_true, prob_pred):
+  	# Find loss for each observation
+    terms = y_true * np.log(prob_pred) + (1 - y_true) * np.log(1 - prob_pred)
+    # Find mean over all observations
+    err = np.mean(terms) 
+    return -err
+
+print('Sklearn LogLoss: {:.5f}'.format(log_loss(y_classification_true, y_classification_pred)))
+print('Your LogLoss: {:.5f}'.format(own_logloss(y_classification_true, y_classification_pred)))
+
+
+
+# Shapes of train and test data
+print('Train shape:', train.shape)
+print('Test shape:', test.shape)
+
+# Train head()
+print(train.head())
+
+# Describe the target variable
+print(train.fare_amount.describe())
+
+# Train distribution of passengers within rides
+print(train.passenger_count.value_counts())
+
+
+# Calculate the ride distance
+train['distance_km'] = haversine_distance(train)
+
+# Draw a scatterplot
+plt.scatter(x=train['fare_amount'], y=train['distance_km'], alpha=0.5)
+plt.xlabel('Fare amount')
+plt.ylabel('Distance, km')
+plt.title('Fare amount based on the distance')
+
+# Limit on the distance
+plt.ylim(0, 50)
+plt.show()
+
+
+
+# Create hour feature
+train['pickup_datetime'] = pd.to_datetime(train.pickup_datetime)
+train['hour'] = train.pickup_datetime.dt.hour
+
+# Find median fare_amount for each hour
+hour_price = train.groupby('hour', as_index=False)['fare_amount'].median()
+
+# Plot the line plot
+plt.plot(hour_price['hour'], hour_price['fare_amount'], marker='o')
+plt.xlabel('Hour of the day')
+plt.ylabel('Median fare amount')
+plt.title('Fare amount based on day time')
+plt.xticks(range(24))
+plt.show()
+
+# Import KFold
+from sklearn.model_selection import KFold
+
+# Create a KFold object
+kf = KFold(n_splits=3, shuffle=True, random_state=123)
+
+# Loop through each split
+fold = 0
+for train_index, test_index in kf.split(train):
+    # Obtain training and testing folds
+    cv_train, cv_test = train.iloc[train_index], train.iloc[test_index]
+    print('Fold: {}'.format(fold))
+    print('CV train shape: {}'.format(cv_train.shape))
+    print('Medium interest listings in CV train: {}\n'.format(sum(cv_train.interest_level == 'medium')))
+    fold += 1
+
+
+
+# Import StratifiedKFold
+from sklearn.model_selection import StratifiedKFold
+
+# Create a StratifiedKFold object
+str_kf = StratifiedKFold(n_splits=3, shuffle=True, random_state=123)
+
+# Loop through each split
+fold = 0
+for train_index, test_index in str_kf.split(train, train['interest_level']):
+    # Obtain training and testing folds
+    cv_train, cv_test = train.iloc[train_index], train.iloc[test_index]
+    print('Fold: {}'.format(fold))
+    print('CV train shape: {}'.format(cv_train.shape))
+    print('Medium interest listings in CV train: {}\n'.format(sum(cv_train.interest_level == 'medium')))
+    fold += 1
+
+
+
+
+
+
+filter_value = 50
+trans_df = walmart[walmart['group']==filter_value]
+trans_df = trans_df.drop('group', axis=1)
+trans_df2 = trans_df.T
+trans_df2.columns = trans_df2.iloc[0]
+trans_df2 = trans_df2.reset_index()
+trans_df2 = trans_df2.drop([0, 1])
+trans_df2 = trans_df2.drop(['index'], axis=1)
+
+trans_df2
+    
