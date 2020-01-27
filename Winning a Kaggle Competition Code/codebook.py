78ad92ffa5272e1123a3bdeca5553868bb42d2e6
Bryan Bumgardner <bryanbumgardner@gmail.com>
2020-01-27 08:51:05 -0500

    adding the first titanic notebook

--- Winning a Kaggle Competition Code/codebook.py
+++ Winning a Kaggle Competition Code/codebook.py
@@ -205,3 +205,197 @@ trans_df2 = trans_df2.drop(['index'], axis=1)
 
 trans_df2
     
+
+
+
+# Create TimeSeriesSplit object
+time_kfold = TimeSeriesSplit(n_splits=3)
+
+# Sort train data by date
+train = train.sort_values('date')
+
+# Iterate through each split
+fold = 0
+for train_index, test_index in time_kfold.split(train):
+    cv_train, cv_test = train.iloc[train_index], train.iloc[test_index]
+    
+    print('Fold :', fold)
+    print('Train date range: from {} to {}'.format(cv_train.date.min(), cv_train.date.max()))
+    print('Test date range: from {} to {}\n'.format(cv_test.date.min(), cv_test.date.max()))
+    fold += 1
+
+
+from sklearn.model_selection import TimeSeriesSplit
+import numpy as np
+
+# Sort train data by date
+train = train.sort_values('date')
+
+# Initialize 3-fold time cross-validation
+kf = TimeSeriesSplit(n_splits=3)
+
+# Get MSE scores for each cross-validation split
+mse_scores = get_fold_mse(train, kf)
+
+print('Mean validation MSE: {:.5f}'.format(np.mean(mse_scores)))
+print('MSE by fold: {}'.format(mse_scores))
+print('Overall validation MSE: {:.5f}'.format(np.mean(mse_scores) + np.std(mse_scores)))
+
+
+
+
+
+
+# FEATURE ENGINEERING
+
+# Look at the initial RMSE
+print('RMSE before feature engineering:', get_kfold_rmse(train))
+
+# Find the total area of the house
+train['TotalArea'] = train['TotalBsmtSF'] + train['FirstFlrSF'] + train['SecondFlrSF']
+print('RMSE with total area:', get_kfold_rmse(train))
+
+# Find the area of the garden
+train['GardenArea'] = train['LotArea'] - train['FirstFlrSF']
+print('RMSE with garden area:', get_kfold_rmse(train))
+
+# Find total number of bathrooms
+train['TotalBath'] = train['FullBath'] + train['HalfBath']
+print('RMSE with number of bathrooms:', get_kfold_rmse(train))
+
+
+# Taxi Fare Prediction Data
+# Concatenate train and test together
+taxi = pd.concat([train, test])
+
+# Convert pickup date to datetime object
+taxi['pickup_datetime'] = pd.to_datetime(taxi['pickup_datetime'])
+
+# Create a day of week feature
+taxi['dayofweek'] = taxi['pickup_datetime'].dt.dayofweek
+
+# Create an hour feature
+taxi['hour'] = taxi['pickup_datetime'].dt.hour
+
+# Split back into train and test
+new_train = taxi[taxi['id'].isin(train['id'])]
+new_test = taxi[taxi['id'].isin(test['id'])]
+
+
+# Feature Engineering on Categorical Variables
+
+# Concatenate train and test together
+houses = pd.concat([train, test])
+
+# Label encoder
+from sklearn.preprocessing import LabelEncoder
+le = LabelEncoder()
+
+# Create new features
+houses['RoofStyle_enc'] = le.fit_transform(houses['RoofStyle'])
+houses['CentralAir_enc'] = le.fit_transform(houses['CentralAir'])
+
+# Look at new features
+print(houses[['RoofStyle', 'RoofStyle_enc', 'CentralAir', 'CentralAir_enc']].head())
+
+
+# Concatenate train and test together
+houses = pd.concat([train, test])
+
+# Label encode binary 'CentralAir' feature
+from sklearn.preprocessing import LabelEncoder
+le = LabelEncoder()
+houses['CentralAir_enc'] = le.fit_transform(houses['CentralAir'])
+
+# Create One-Hot encoded features
+ohe = pd.get_dummies(houses['RoofStyle'], prefix='RoofStyle')
+
+# Concatenate OHE features to houses
+houses = pd.concat([houses, ohe], axis=1)
+
+# Look at OHE features
+print(houses[[col for col in houses.columns if 'RoofStyle' in col]].head(3))
+
+
+
+# Target Encoding
+# High Cardinality - categorical features with a lot of categories
+# At least ten
+
+
+
+
+
+def test_mean_target_encoding(train, test, target, categorical, alpha=5):
+    # Calculate global mean on the train data
+    global_mean = train[target].mean()
+    
+    # Group by the categorical feature and calculate its properties
+    train_groups = train.groupby(categorical)
+    category_sum = train_groups[target].sum()
+    category_size = train_groups.size()
+    
+    # Calculate smoothed mean target statistics
+    train_statistics = (category_sum + global_mean * alpha) / (category_size + alpha)
+    
+    # Apply statistics to the test data and fill new categories
+    test_feature = test[categorical].map(train_statistics).fillna(global_mean)
+    return test_feature.values
+
+
+
+def train_mean_target_encoding(train, target, categorical, alpha=5):
+    # Create 5-fold cross-validation
+    kf = KFold(n_splits=5, random_state=123, shuffle=True)
+    train_feature = pd.Series(index=train.index)
+    
+    # For each folds split
+    for train_index, test_index in kf.split(train):
+        cv_train, cv_test = train.iloc[train_index], train.iloc[test_index]
+      
+        # Calculate out-of-fold statistics and apply to cv_test
+        cv_test_feature = test_mean_target_encoding(cv_train, cv_test, target, categorical, alpha)
+        
+        # Save new feature for this particular fold
+        train_feature.iloc[test_index] = cv_test_feature       
+    return train_feature.values
+
+
+
+def mean_target_encoding(train, test, target, categorical, alpha=5):
+  
+    # Get the train feature
+    train_feature = train_mean_target_encoding(train, target, categorical, alpha)
+  
+    # Get the test feature
+    test_feature = test_mean_target_encoding(train, test, target, categorical, alpha)
+    
+    # Return new features to add to the model
+    return train_feature, test_feature
+
+
+"""
+Great! Now you are equipped with a function that performs mean target encoding of any categorical feature. Move on to learn how to implement mean target encoding for the K-fold cross-validation using the mean_target_encoding() function you've just built!
+"""
+
+
+# k-fold cross validations
+# from some data about Kobe
+
+# Create 5-fold cross-validation
+kf = KFold(n_splits=5, random_state=123, shuffle=True)
+
+# For each folds split
+for train_index, test_index in kf.split(bryant_shots):
+    cv_train, cv_test = bryant_shots.iloc[train_index], bryant_shots.iloc[test_index]
+
+    # Create mean target encoded feature
+    cv_train['game_id_enc'], cv_test['game_id_enc'] = mean_target_encoding(train=cv_train,
+                                                                           test=cv_test,
+                                                                           target='shot_made_flag',
+                                                                           categorical='game_id',
+                                                                           alpha=5)
+    # Look at the encoding
+    print(cv_train[['game_id', 'shot_made_flag', 'game_id_enc']].sample(n=1))
+
+    
